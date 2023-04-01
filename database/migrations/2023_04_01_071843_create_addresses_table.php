a3e962a06500bad8043162fef62b107d81196dbf
Jon <joncaesar@gmail.com>
2023-04-01 16:01:03 +0800

    Create 2023_04_01_071843_create_addresses_table.php

---
+++ database/migrations/2023_04_01_071843_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id('address_id');
+            $table->string('house_no');
+            $table->string('street');
+            $table->string('brangay');
+            $table->string('subdivision');
+            $table->string('city');
+            $table->string('created_by');
+            $table->string('created_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
