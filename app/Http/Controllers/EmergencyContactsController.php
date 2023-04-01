8082e39ca6ba6093f5267ac16ff294963f32b35a
Jon <joncaesar@gmail.com>
2023-04-01 16:00:22 +0800

    Create EmergencyContactsController.php

---
+++ app/Http/Controllers/EmergencyContactsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmergencyContacts;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmergencyContactsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmergencyContacts $emergencyContacts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmergencyContacts $emergencyContacts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EmergencyContacts $emergencyContacts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmergencyContacts $emergencyContacts)
+    {
+        //
+    }
+}
