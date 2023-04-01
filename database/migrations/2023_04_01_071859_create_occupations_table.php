c95f70815800734f1737b54bcc0c85fdac8ac1f1
Jon <joncaesar@gmail.com>
2023-04-01 16:27:56 +0800

    Update 2023_04_01_071859_create_occupations_table.php

--- database/migrations/2023_04_01_071859_create_occupations_table.php
+++ database/migrations/2023_04_01_071859_create_occupations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('occupations', function (Blueprint $table) {
+        Schema::create('tbloccupations', function (Blueprint $table) {
             $table->id('occupation_id');
             $table->string('occupation_name');
             $table->string('specialty');
