507550b952cff829675b13472ead0580010a2eec
Jon <joncaesar@gmail.com>
2023-04-01 16:00:49 +0800

    Create Emails.php

---
+++ app/Models/Emails.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Emails extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tblemails';
+    protected $fillable = [
+        'email',
+        'created_by',
+        'created_on'
+    ];
+
+    protected $timestamps = false;
+}
