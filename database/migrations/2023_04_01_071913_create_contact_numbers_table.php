0e38c58ffe2dfdf47eadb9bd5c461749dc6b7496
Jon <joncaesar@gmail.com>
2023-04-01 16:27:59 +0800

    Update 2023_04_01_071913_create_contact_numbers_table.php

--- database/migrations/2023_04_01_071913_create_contact_numbers_table.php
+++ database/migrations/2023_04_01_071913_create_contact_numbers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contact_numbers', function (Blueprint $table) {
+        Schema::create('tblcontact_numbers', function (Blueprint $table) {
             $table->id('contactNumber_id');
             $table->string('mobile');
             $table->string('created_by');
