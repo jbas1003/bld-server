eb95d16ff217e6fc8bab41302c97f484268ba702
Jon <joncaesar@gmail.com>
2023-04-01 16:00:38 +0800

    Create ContactInfo.php

---
+++ app/Models/ContactInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactInfo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tblcontact_infos';
+    protected $fillable = [
+        'member_id',
+        'address_id',
+        'contactNumber_id',
+        'email_id',
+        'occupation_id',
+        'created_by',
+        'created_on'
+    ];
+
+    protected $timestamps = false;
+}
