1c8cdb5484f523a84eb628766438f6009dc6446d
Jon <joncaesar@gmail.com>
2023-04-01 16:00:01 +0800

    Create AddressesController.php

---
+++ app/Http/Controllers/AddressesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Addresses;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddressesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Addresses $addresses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Addresses $addresses)
+    {
+        //
+    }
+}
