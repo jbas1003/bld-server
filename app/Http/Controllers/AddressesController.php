e7627465ae643042bcee835f3ab95f06f0e5984a
Jon <joncaesar@gmail.com>
2023-04-02 20:35:53 +0800

    Update AddressesController.php

--- app/Http/Controllers/AddressesController.php
+++ app/Http/Controllers/AddressesController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Addresses;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AddressesController extends Controller
 {
@@ -29,7 +30,43 @@ class AddressesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'address_line1' => 'nullable|string|max:191',
+                'address_line2' => 'nullable|string|max:255',
+                'city' => 'nullable|string|max:191',
+                'created_by' => 'required|string|max:191',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 422,
+                    'errors' => $validator->messages(),
+                ], 422);
+            } else {
+                $address = Addresses::create([
+                    'address_line1' => $request->address_line1,
+                    'address_line2' => $request->address_line2,
+                    'city' => $request->city,
+                    'created_by' => $request->created_by,
+                    'created_on' => now(),
+                ]);
+
+                if ($address) {
+                    return response()->json([
+                        'status' => 200,
+                        'address' => $address->id
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'status' => 500,
+                        'Error' => 'Server Error!'
+                    ], 500);
+                }
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
