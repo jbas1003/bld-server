9b375fe63191ab632d9b1904bebf303679972584
Jon <joncaesar@gmail.com>
2023-04-02 20:36:22 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailsController;
 use App\Http\Controllers\MembersController;
+use App\Http\Controllers\AddressesController;
+use App\Http\Controllers\OccupationController;
+use App\Http\Controllers\ContactNumbersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +23,37 @@ use App\Http\Controllers\MembersController;
 //     return $request->user();
 // });
 
+// START: Members Routes
+
 Route::get('members', [MembersController::class, 'show']);
 Route::post('members', [MembersController::class, 'store']);
+
+// END: Members Routes
+
+// START: Addresses Routes
+
+Route::get('addresses', [AddressesController::class, 'show']);
+Route::post('addresses', [AddressesController::class, 'store']);
+
+// END: Addresses Routes
+
+// START: Occupations Routes
+
+Route::get('occupations', [OccupationController::class, 'show']);
+Route::post('occupations', [OccupationController::class, 'store']);
+
+// END: Occupations Routes
+
+// START: Contact Numbers Routes
+
+Route::get('contact_numbers', [ContactNumbersController::class, 'show']);
+Route::post('contact_numbers', [ContactNumbersController::class, 'store']);
+
+// END: Contact Numbers Routes
+
+// START: Emails Routes
+
+Route::get('emails', [EmailsController::class, 'show']);
+Route::post('emails', [EmailsController::class, 'store']);
+
+// END: Emails Routes
