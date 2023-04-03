51d311586534f0937ca20a1002234b8590afef3c
Jon <joncaesar@gmail.com>
2023-04-03 20:54:11 +0800

    Update Occupation.php

--- app/Models/Occupation.php
+++ app/Models/Occupation.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\ContactInfo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Occupation extends Model
 {
@@ -22,4 +23,8 @@ class Occupation extends Model
     ];
 
     public $timestamps = false;
+
+    public function contactInfo ():BelongsTo {
+        return $this->belongsTo(ContactInfo::class);
+    }
 }
