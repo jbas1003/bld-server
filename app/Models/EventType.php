e80514babb15c89cb54cf638935c06399f834245
Jon <joncaesar@gmail.com>
2023-04-10 20:37:00 +0800

    Update EventType.php

--- app/Models/EventType.php
+++ app/Models/EventType.php
@@ -10,6 +10,7 @@ class EventType extends Model
     use HasFactory;
 
     protected $table = 'tblevent_types';
+    protected $primaryKey = 'event_type_id';
     protected $fillable = [
         'event_type_name',
         'event_type_category',
