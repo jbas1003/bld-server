f6908508bfc90ca2c8bb1bc94c4640ad60980711
Jon <joncaesar@gmail.com>
2023-04-15 23:47:42 +0800

    Update AttendanceController.php

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AttendanceController extends Controller
 {
@@ -29,15 +30,94 @@ class AttendanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(),[
+                'member_id' => 'required|integer',
+                'event_id' => 'required|integer',
+                'status' => 'nullable|string',
+                'created_by' => 'required|integer'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 422,
+                    'message' => $validator->messages()
+                ]);
+            } else {
+                $memberExist = Attendance::where('member_id', $request->member_id)
+                                        ->where('event_id', $request->event_id)
+                                        ->where('status', $request->status)
+                                        ->get();
+
+                if ($memberExist->count() > 0) {
+                    return response()->json([
+                        'status' => 422,
+                        'message' => 'This person already has an attendance record for this event.'
+                    ]);
+                } else {
+                    
+                    $attendance = Attendance::create([
+                        'member_id' => $request->member_id,
+                        'event_id' => $request->event_id,
+                        'status' => $request->status,
+                        'created_by' => $request->created_by,
+                        'created_on' => now()
+                    ]);
+
+                    if ($attendance) {
+                        return response()->json([
+                            'status' => 200,
+                            'message' => 'Attendance record added successfully!'
+                        ]);
+                    } else {
+                        return response()->json([
+                            'status' => 422,
+                            'message' => 'A problem was encountered while creating attendance record. Please contact system administrator.'
+                        ]);
+                    }
+                }
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Attendance $attendance)
+    public function show(Request $request)
     {
-        //
+        // 
+    }
+
+    public function showAttendance(Request $request) {
+        try {
+            // $getAttendance = Attendance::join('tblmembers', 'tblattendances.member_id', '=', 'tblmembers.member_id')
+            //                 ->join('tblevents', 'tblattendances.event_id', '=', 'tblevents.event_id')
+            //                 ->select('tblattendances.attendance_id', 'tblattendances.member_id', 'tblattendances.event_id',
+            //                         'tblattendances.status', 'tblevents.event_type_id')
+            //                 ->get();
+
+            $getAttendance = Attendance::where('tblattendances.event_id', $request->event_id)
+                            ->join('tblevents', 'tblattendances.event_id', '=', 'tblevents.event_id')
+                            ->select('tblattendances.attendance_id', 'tblattendances.member_id', 'tblattendances.event_id',
+                                    'tblattendances.status', 'tblevents.event_type_id')
+                            ->get();
+            
+            if ($getAttendance->count() > 0) {
+                return response()->json([
+                    'status' => 200,
+                    'body' => $getAttendance
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 422,
+                    'message' => 'No attendance records yet.'
+                ]);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
