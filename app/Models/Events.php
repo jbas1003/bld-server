bfb0efcbe3f01c499a825691eadd887ad7ec4fbc
Jon <joncaesar@gmail.com>
2023-04-18 00:02:01 +0800

    Update Events.php

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -14,6 +14,7 @@ class Events extends Model
     protected $fillable = [
         'event_name',
         'event_subtitle',
+        'location',
         'start_date',
         'end_date',
         'status',
