ad9196460c87b03dbe0ae1f338c9e6afbf9865ec
Jon <joncaesar@gmail.com>
2023-04-22 16:04:17 +0800

    Create 2023_04_22_074606_create_invites_table.php

---
+++ database/migrations/2023_04_22_074606_create_invites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tblinvites', function (Blueprint $table) {
+            $table->id('invite_id');
+            $table->integer('event_encounter_id')->nullable();
+            $table->integer('inviter_id')->nullable();
+            $table->string('relationship')->nullable();
+            $table->integer('created_by');
+            $table->string('created_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tblinvites');
+    }
+};
