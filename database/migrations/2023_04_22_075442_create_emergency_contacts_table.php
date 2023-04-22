58e093a83c377e3543fd6bc02742ea962de30ebc
Jon <joncaesar@gmail.com>
2023-04-22 16:04:20 +0800

    Create 2023_04_22_075442_create_emergency_contacts_table.php

---
+++ database/migrations/2023_04_22_075442_create_emergency_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tblemergency_contacts', function (Blueprint $table) {
+            $table->id('emergencyContact_id');
+            $table->integer('seId');
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('relationship')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('email')->nullable();
+            $table->integer('created_by');
+            $table->string('created_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tblemergency_contacts');
+    }
+};
