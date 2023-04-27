1a495371551a497b09f1997503c13f51316716dd
Jon <joncaesar@gmail.com>
2023-04-27 11:22:36 +0800

    Update SinglesEncounterController.php

--- app/Http/Controllers/SinglesEncounterController.php
+++ app/Http/Controllers/SinglesEncounterController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\EmergencyContact;
 use App\Models\SinglesEncounter;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SinglesEncounterController extends Controller
 {
@@ -29,7 +32,83 @@ class SinglesEncounterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                // // START: Table Members Data
+
+                //     'first_name' => 'required|string|max:191',
+                //     'middle_name' => 'required|string|max:191',
+                //     'last_name' => 'required|string|max:191',
+                //     'nickname' => 'nullable|string|max:191',
+                //     'birthday' => 'nullable|string|max:191',
+                //     'gender' => 'nullable|string|max:90',
+                //     'civil_status' => 'nullable|string|max:50',
+                //     'religion' => 'nullable|string|max:191',
+                //     'baptism' => 'nullable|string|max:30',
+                //     'confirmation' => 'nullable|string|max:30',
+                //     'member_status' => 'nullable|integer',
+                //     'created_by' => 'required|integer',
+
+                // // END: Table Members Data
+
+                // // START: Table Address Data
+
+                //     'member_addressLine1' => 'nullable|string|max:255',
+                //     'member_addressLine2' => 'nullable|string|max:255',
+                //     'member_city' => 'nullalbe|string|max:255',
+
+                // // END: Table Address Data
+
+                // // START: Table ContactNumbers Data
+
+                //     'member_mobile' => 'nullable|string|max:20',
+
+                // // END: Table ContactNumbers Data
+
+                // // START: Table Emails Data
+
+                //     'member_email' => 'nullable|string|max:191',
+
+                // // END: Table Emails Data
+
+                // // Start: Table Occupations Data
+
+                //     'occupation' => 'nullable|string|max:191',
+                //     'specialty' => 'nullable|string|max:191',
+                //     'company' => 'nullable|string|max:191',
+                //     'company_addressLine1' => 'nullable|string|max:255',
+                //     'company_addressLine2' => 'nullable|string|max:255',
+                //     'city' => 'nullable|string|max:191',
+
+                // // END: Table Occupations Data
+
+                // START: Table EmergencyContacts Data
+
+                    'emergency_contacts' => 'nullable|array'
+
+                // END: Table EmergencyContacts Data
+
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 422,
+                    'message' => $validator->message(),
+                ]);
+            } else {
+                // $emergecnyContact = EmergencyContact::create([
+
+                // ]);
+                $emergency_contacts = $request->emergency_contacts;
+                
+                return $emergency_contacts->name;
+                // for ($i=0; $i < count($emergency_contacts)-1; $i++) { 
+                //     return $emergency_contacts[i]->name;
+                // }
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -37,7 +116,18 @@ class SinglesEncounterController extends Controller
      */
     public function show(SinglesEncounter $singlesEncounter)
     {
-        //
+        if ($singlesEncounter->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Success!',
+                'body' => $singlesEncounter
+            ]);
+        } else {
+            return response()->json([
+                'status' => 422,
+                'message' => 'No records found.'
+            ]);
+        }
     }
 
     /**
