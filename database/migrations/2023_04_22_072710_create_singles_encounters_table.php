83a4630169edb01587102a11ce07a6bea72b1d49
Jon <joncaesar@gmail.com>
2023-04-27 18:14:58 +0800

    Update 2023_04_22_072710_create_singles_encounters_table.php

--- database/migrations/2023_04_22_072710_create_singles_encounters_table.php
+++ database/migrations/2023_04_22_072710_create_singles_encounters_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tblsingles_encounter', function (Blueprint $table) {
             $table->id('seId');
             $table->integer('member_id')->nullable();
-            $table->integer('emergencyContact_id')->nullable();
             $table->string('room')->nullable();
             $table->string('tribe')->nullable();
             $table->string('nation')->nullable();
