21f7be51bbc5006fdc0491f8527d67ba959b3288
Jon <joncaesar@gmail.com>
2023-04-27 18:15:01 +0800

    Update 2023_04_22_075442_create_emergency_contacts_table.php

--- database/migrations/2023_04_22_075442_create_emergency_contacts_table.php
+++ database/migrations/2023_04_22_075442_create_emergency_contacts_table.php
@@ -11,28 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<< Updated upstream:database/migrations/2023_04_01_072625_create_emergency_contacts_table.php
         Schema::create('tblemergency_contacts', function (Blueprint $table) {
             $table->id('emergencyContact_id');
-<<<<<<< Updated upstream:database/migrations/2023_04_22_075442_create_emergency_contacts_table.php
-            $table->integer('seId');
+            $table->integer('seId')->nullable();
             $table->string('name')->nullable();
             $table->string('mobile')->nullable();
             $table->string('email')->nullable();
-=======
-            $table->integer('emergencyContactInfo_id');
-            $table->integer('member_id');
-=======
-        Schema::create('tblinvites', function (Blueprint $table) {
-            $table->id('invite_id');
-            $table->integer('encounter_id')->nullable();
-            $table->integer('event_id')->nullable();
-            $table->integer('inviter_id')->nullable();
             $table->string('relationship')->nullable();
->>>>>>> Stashed changes:database/migrations/2023_04_22_074606_create_invites_table.php
->>>>>>> Stashed changes:database/migrations/2023_04_01_072625_create_emergency_contacts_table.php
-            $table->integer('created_by');
-            $table->string('created_on');
+            $table->integer('created_by')->nullable();
+            $table->string('created_on')->nullable();
         });
     }
 
