1d2611165a0d1009dd5cc78d06edc3f97bae4126
Jon <joncaesar@gmail.com>
2023-04-29 10:08:40 +0800

    Update SinglesEncounterController.php

--- app/Http/Controllers/SinglesEncounterController.php
+++ app/Http/Controllers/SinglesEncounterController.php
@@ -116,139 +116,139 @@ class SinglesEncounterController extends Controller
                                           ->join('tblcontact_numbers', 'tblcontact_infos.contactNumber_id', '=', 'tblcontact_numbers.contactNumber_id')
                                           ->join('tblemails', 'tblcontact_infos.email_id', '=', 'tblemails.email_id')
                                           ->first();
-                return $memberExist;
-                // if ((($memberExist->first_name === $request->first_name) && ($memberExist->middle_name === $request->middle_name) && ($memberExist->last_name === $request->last_name)) || ($memberExist->email === $request->email) || ($memberExist->mobile === $request->mobile)) {
-                // return response()->json([
-                //     'status' => 422,
-                //     'message' => 'Please check name, email, or mobile number. One of these info might already exist, or might have not existed.'
-                // ], 422);
-                // } else {
-                //     // START: Members Insert Query
+                                          
+                if ((($memberExist->first_name === $request->first_name) && ($memberExist->middle_name === $request->middle_name) && ($memberExist->last_name === $request->last_name)) || ($memberExist->email === $request->email) || ($memberExist->mobile === $request->mobile)) {
+                return response()->json([
+                    'status' => 422,
+                    'message' => 'Please check name, email, or mobile number. One of these info might already exist, or might have not existed.'
+                ], 422);
+                } else {
+                    // START: Members Insert Query
 
-                //         $member = Members::create([
-                //             'first_name' => $request->first_name,
-                //             'middle_name' => $request->middle_name,
-                //             'last_name' => $request->last_name,
-                //             'nickname' => $request->nickname,
-                //             'birthday' => $request->birthday,
-                //             'gender' => $request->gender,
-                //             'civil_status' => $request->civil_status,
-                //             'religion' => $request->religion,
-                //             'baptized' => $request->baptism,
-                //             'confirmed' => $request->confirmation,
-                //             'member_status_id' => $request->member_status_id,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $member = Members::create([
+                            'first_name' => $request->first_name,
+                            'middle_name' => $request->middle_name,
+                            'last_name' => $request->last_name,
+                            'nickname' => $request->nickname,
+                            'birthday' => $request->birthday,
+                            'gender' => $request->gender,
+                            'civil_status' => $request->civil_status,
+                            'religion' => $request->religion,
+                            'baptized' => $request->baptism,
+                            'confirmed' => $request->confirmation,
+                            'member_status_id' => $request->member_status_id,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Members Insert Query
+                    // END: Members Insert Query
 
-                //     // START: Address Insert Query
+                    // START: Address Insert Query
 
-                //         $address = Addresses::create([
-                //             'member_addressLine1' => $request->member_address_line1,
-                //             'member_addressLine2' => $request->member_address_line2,
-                //             'member_city' => $request->member_city,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $address = Addresses::create([
+                            'address_line1' => $request->member_addressLine1,
+                            'address_line2' => $request->member_addressLine2,
+                            'city' => $request->member_city,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Address Insert Query
+                    // END: Address Insert Query
 
-                //     // START: Contact Number Insert Query
+                    // START: Contact Number Insert Query
 
-                //         $contactNumber = ContactNumbers::create([
-                //             'member_mobile' => $request->member_mobile,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $contactNumber = ContactNumbers::create([
+                            'mobile' => $request->member_mobile,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Contact Number Insert Query
+                    // END: Contact Number Insert Query
 
-                //     // START: Email Insert Query
+                    // START: Email Insert Query
 
-                //         $email = Emails::create([
-                //             'member_email' => $request->email,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $email = Emails::create([
+                            'email' => $request->member_email,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Email Insert Query
+                    // END: Email Insert Query
 
-                //     // START: Occupation Insert Query
+                    // START: Occupation Insert Query
 
-                //         $occupation = Occupation::create([
-                //             'occupation' => $request->occupation_name,
-                //             'specialty' => $request->specialty,
-                //             'company' => $request->company,
-                //             'company_addressLine1' => $request->company_address_line1,
-                //             'company_addressLine2' => $request->company_address_line2,
-                //             'company_city' => $request->city,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $occupation = Occupation::create([
+                            'occupation_name' => $request->occupation,
+                            'specialty' => $request->specialty,
+                            'company' => $request->company,
+                            'address_line1' => $request->company_addressLine1,
+                            'address_line2' => $request->company_addressLine2,
+                            'city' => $request->city,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Occupation Insert Query
+                    // END: Occupation Insert Query
 
-                //     // START: Contact Info Insert Query
+                    // START: Contact Info Insert Query
 
-                //         $contactInfo = ContactInfo::create([
-                //             'member_id' => $member->member_id,
-                //             'address_id' => $address->id,
-                //             'contactNumber_id' => $contactNumber->id,
-                //             'email_id' => $email->id,
-                //             'occupation_id' => $occupation->id,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $contactInfo = ContactInfo::create([
+                            'member_id' => $member->member_id,
+                            'address_id' => $address->id,
+                            'contactNumber_id' => $contactNumber->id,
+                            'email_id' => $email->id,
+                            'occupation_id' => $occupation->id,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Contact Info Insert Query
+                    // END: Contact Info Insert Query
 
-                //     // START: Singles Encounter Insert Query
+                    // START: Singles Encounter Insert Query
 
-                //         $SE = SinglesEncounter::create([
-                //             'member_id' => $member->member_id,
-                //             'room' => $request->room,
-                //             'nation' => $request->nation,
-                //             'created_by' => $request->created_by,
-                //             'created_on' => now()
-                //         ]);
+                        $SE = SinglesEncounter::create([
+                            'member_id' => $member->member_id,
+                            'room' => $request->room,
+                            'nation' => $request->nation,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
 
-                //     // END: Singles Encounter Insert Query
+                    // END: Singles Encounter Insert Query
 
-                //     // START: Emergency Contact Insert Query
+                    // START: Emergency Contact Insert Query
 
-                //         $dataSet = [];
-                //         $emergency_contacts = $request->emergency_contacts;
+                        $dataSet = [];
+                        $emergency_contacts = $request->emergency_contacts;
                         
-                //         foreach ($emergency_contacts as $contacts) {
-                //             $dataSet[] = [
-                //                 'seId' => $SE->seId,
-                //                 'name' => $contacts['name'],
-                //                 'mobile' => $contacts['mobile'],
-                //                 'email' => $contacts['email'],
-                //                 'relationship' => $contacts['relationship'],
-                //                 'created_by' => $contacts['created_by'],
-                //                 'created_on' => now()
-                //             ];
-                //         }
+                        foreach ($emergency_contacts as $contacts) {
+                            $dataSet[] = [
+                                'seId' => $SE->seId,
+                                'name' => $contacts['name'],
+                                'mobile' => $contacts['mobile'],
+                                'email' => $contacts['email'],
+                                'relationship' => $contacts['relationship'],
+                                'created_by' => $contacts['created_by'],
+                                'created_on' => now()
+                            ];
+                        }
 
-                //         $emergencyContacts = DB::table('tblemergency_contacts')->insert($dataSet);
+                        $emergencyContacts = DB::table('tblemergency_contacts')->insert($dataSet);
 
-                //     // END: Emergency Contact Insert Query
-
-                //     if ($contactInfo === true & $emergencyContacts === true) {
-                //         return response()->json([
-                //             'status' => 200,
-                //             'message' => 'Adding participant was successful!'
-                //         ]);
-                //     } else {
-                //         return response()->json([
-                //             'status' => 500,
-                //             'message' => 'A problem was encountered while adding participant records. Please acontact system administrators.'
-                //         ]);
-                //     }
-                // }
+                    // END: Emergency Contact Insert Query
+                    
+                    if ($contactInfo->count() > 0 & $emergencyContacts === true) {
+                        return response()->json([
+                            'status' => 200,
+                            'message' => 'Adding participant was successful!'
+                        ]);
+                    } else {
+                        return response()->json([
+                            'status' => 500,
+                            'message' => 'A problem was encountered while adding participant records. Please acontact system administrators.'
+                        ]);
+                    }
+                }
             }
         } catch (\Throwable $th) {
             // return response()->json([
