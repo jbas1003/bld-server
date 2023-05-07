ae749a8ffcd30e190f9f7937c2dede989fa3ff99
Jon <joncaesar@gmail.com>
2023-05-07 19:43:25 +0800

    Update SinglesEncounterController.php

--- app/Http/Controllers/SinglesEncounterController.php
+++ app/Http/Controllers/SinglesEncounterController.php
@@ -280,7 +280,7 @@ class SinglesEncounterController extends Controller
     }
 
     public function showSE (Request $request) {
-        $participants = Members::select('tblmembers.member_id', 'tblmembers.first_name', 'tblmembers.middle_name',
+        $participants = Members::select('tblmembers.member_id', 'tblsingles_encounter.seId', 'tblmembers.first_name', 'tblmembers.middle_name',
                                         'tblmembers.last_name', 'tblmembers.nickname', 'tblmembers.gender',
                                         'tblmembers.birthday', 'tblmembers.civil_status', 'tblmembers.religion',
                                         'tblmembers.baptism', 'tblmembers.confirmation', 'tbladdresses.address_line1',
@@ -289,16 +289,14 @@ class SinglesEncounterController extends Controller
                                         'tbloccupations.company', 'tbloccupations.address_line1 As work_addressLine1',
                                         'tbloccupations.address_line2 as work_addressLine2', 'tbloccupations.city As work_city',
                                         'tblsingles_encounter.status As attendance_status')
-                            ->join('tblcontact_infos', 'tblmembers.member_id', '=', 'tblcontact_infos.member_id')
-                            ->join('tbladdresses', 'tblcontact_infos.address_id', '=', 'tbladdresses.address_id')
-                            ->join('tblcontact_numbers', 'tblcontact_infos.contactNumber_id', '=', 'tblcontact_numbers.contactNumber_id')
-                            ->join('tblemails', 'tblcontact_infos.email_id', '=', 'tblemails.email_id')
-                            ->join('tbloccupations', 'tblcontact_infos.occupation_id', '=', 'tbloccupations.occupation_id')
+                            ->leftJoin('tblcontact_infos', 'tblmembers.member_id', '=', 'tblcontact_infos.member_id')
+                            ->leftJoin('tbladdresses', 'tblcontact_infos.address_id', '=', 'tbladdresses.address_id')
+                            ->leftJoin('tblcontact_numbers', 'tblcontact_infos.contactNumber_id', '=', 'tblcontact_numbers.contactNumber_id')
+                            ->leftJoin('tblemails', 'tblcontact_infos.email_id', '=', 'tblemails.email_id')
+                            ->leftJoin('tbloccupations', 'tblcontact_infos.occupation_id', '=', 'tbloccupations.occupation_id')
                             ->leftJoin('tblsingles_encounter', 'tblmembers.member_id', '=', 'tblsingles_encounter.member_id')
                             ->where('tblmembers.civil_status', 'LIKE', '%single')
-                            ->when($request->event, function($se) use ($request) {
-                                $se->where('tblsingles_encounter.event_id', '=', $request->event);
-                            })
+                            // ->whereOr('tblsingles_encounter.event_id', '=', $request->event)
                             ->with(['emergency_contacts' => function($query) {
                                 $query->select('tblemergency_contacts.name',
                                                 'tblemergency_contacts.mobile',
@@ -334,7 +332,247 @@ class SinglesEncounterController extends Controller
      */
     public function update(Request $request, SinglesEncounter $singlesEncounter)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                // START: Table Members Data
+
+                    'first_name' => 'nullable|string|max:191',
+                    'middle_name' => 'nullable|string|max:191',
+                    'last_name' => 'nullable|string|max:191',
+                    'nickname' => 'nullable|string|max:191',
+                    'birthday' => 'nullable|string|max:191',
+                    'gender' => 'nullable|string|max:90',
+                    'civil_status' => 'nullable|string|max:50',
+                    'religion' => 'nullable|string|max:191',
+                    'baptized' => 'nullable|string|max:30',
+                    'confirmed' => 'nullable|string|max:30',
+                    'member_status_id' => 'nullable|integer',
+
+                // END: Table Members Data
+
+                // START: Table Address Data
+
+                    'member_addressLine1' => 'nullable|string|max:255',
+                    'member_addressLine2' => 'nullable|string|max:255',
+                    'member_city' => 'nullable|string|max:255',
+
+                // END: Table Address Data
+
+                // START: Table ContactNumbers Data
+
+                    'member_mobile' => 'nullable|string|max:20',
+
+                // END: Table ContactNumbers Data
+
+                // START: Table Emails Data
+
+                    'member_email' => 'nullable|string|max:191',
+
+                // END: Table Emails Data
+
+                // Start: Table Occupations Data
+
+                    'occupation' => 'nullable|string|max:191',
+                    'specialty' => 'nullable|string|max:191',
+                    'company' => 'nullable|string|max:191',
+                    'company_addressLine1' => 'nullable|string|max:255',
+                    'company_addressLine2' => 'nullable|string|max:255',
+                    'city' => 'nullable|string|max:191',
+
+                // END: Table Occupations Data
+
+                // START: Table EmergencyContacts Data
+
+                    'emergency_contacts' => 'nullable|array',
+
+                // END: Table EmergencyContacts Data
+
+                // START: Table Singles Encounter Data
+
+                    'room' => 'nullable|string|max:50',
+                    'tribe' => 'nullable|string|max:50',
+                    'nation' => 'nullable|string|max:50',
+                    'event_id' => 'nullable|integer',
+                    'status' => 'nullable|string|max:50',
+
+                // END: Table Singles Encounter Data
+
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 422,
+                    'message' => $validator->messages(),
+                ]);
+            } else {
+                // START: Members Update Query
+                    $getMember = ContactInfo::where('member_id', $request->member_id)->first();
+                    
+                    $member = Members::where('member_id', $getMember->member_id)
+                        ->update([
+                            'first_name' => $request->first_name,
+                            'middle_name' => $request->middle_name,
+                            'last_name' => $request->last_name,
+                            'nickname' => $request->nickname,
+                            'birthday' => $request->birthday,
+                            'gender' => $request->gender,
+                            'civil_status' => $request->civil_status,
+                            'religion' => $request->religion,
+                            'baptism' => $request->baptism,
+                            'confirmation' => $request->confirmation,
+                            'member_status_id' => $request->member_status_id,
+                        ]);
+
+                // END: Members Update Query
+
+                // START: Address Update Query
+
+                    $address = Addresses::where('address_id', $getMember->address_id)
+                        ->update([
+                            'address_line1' => $request->member_addressLine1,
+                            'address_line2' => $request->member_addressLine2,
+                            'city' => $request->member_city,
+                        ]);
+
+                // END: Address Update Query
+
+                // START: Contact Number Update Query
+
+                    $contactNumber = ContactNumbers::where('contactNumber_id', $getMember->contactNumber_id)
+                        ->update([
+                            'mobile' => $request->member_mobile,
+                        ]);
+
+                // END: Contact Number Update Query
+
+                // START: Email Update Query
+
+                    $email = Emails::where('email_id', $getMember->email_id)
+                        ->update([
+                            'email' => $request->member_email,
+                        ]);
+
+                // END: Email Update Query
+
+                // START: Occupation Update Query
+
+                    $occupation = Occupation::where('occupation_id', $getMember->occupation_id)
+                        ->update([
+                        'occupation_name' => $request->occupation,
+                        'specialty' => $request->specialty,
+                        'company' => $request->company,
+                        'address_line1' => $request->company_addressLine1,
+                        'address_line2' => $request->company_addressLine2,
+                        'city' => $request->city,
+                    ]);
+
+                // END: Occupation Update Query
+
+                // START: Singles Encounter Update Query
+                    $getSe = SinglesEncounter::where('member_id', $getMember->member_id)->first();
+                    $dataSet = [];
+                    $emergency_contacts = $request->emergency_contacts;
+
+                    if ($getSe) {
+                        $SE = SinglesEncounter::where('seId', $getSe->seId)
+                            ->update([
+                                'room' => $request->room,
+                                'tribe' => $request->tribe,
+                                'nation' => $request->nation,
+                                'event_id' => $request->event_id,
+                                'status' => $request->status,
+                            ]);
+
+                            $getEmergencyContacts = EmergencyContact::where('seId', $getSe->seId)->get();
+                            
+                            if ($getEmergencyContacts->count() > 0) {
+                                foreach ($emergencyContacts as $contacts) {
+                                    // update emergency contacts
+                                }
+                            } else {
+                                // START: Emergency Contact Insert Query
+
+                                    foreach ($emergency_contacts as $contacts) {
+                                        $dataSet[] = [
+                                            'seId' => $getSe->seId,
+                                            'name' => $contacts['name'],
+                                            'mobile' => $contacts['mobile'],
+                                            'email' => $contacts['email'],
+                                            'relationship' => $contacts['relationship'],
+                                            'created_by' => $request->created_by,
+                                            'created_on' => now()
+                                        ];
+                                    }
+
+                                    $emergencyContacts = DB::table('tblemergency_contacts')->insert($dataSet);
+
+                                // END: Emergency Contact Insert Query
+                            }
+
+                            // if ($getEmergencyContacts) {
+                                
+                            // } else {
+                                
+                            // }
+                            
+                    } else {
+                        $SE = SinglesEncounter::create([
+                            'member_id' => $getMember->member_id,
+                            'room' => $request->room,
+                            'tribe' => $request->tribe,
+                            'nation' => $request->nation,
+                            'event_id' => $request->event_id,
+                            'status' => $request->status,
+                            'created_by' => $request->created_by,
+                            'created_on' => now()
+                        ]);
+
+                        return $SE->seId;
+
+                        // // START: Emergency Contact Insert Query
+
+                        //     $dataSet = [];
+                        //     $emergency_contacts = $request->emergency_contacts;
+                            
+                        //     foreach ($emergency_contacts as $contacts) {
+                        //         $dataSet[] = [
+                        //             'seId' => $SE->seId,
+                        //             'name' => $contacts['name'],
+                        //             'mobile' => $contacts['mobile'],
+                        //             'email' => $contacts['email'],
+                        //             'relationship' => $contacts['relationship'],
+                        //             'created_by' => $contacts['created_by'],
+                        //             'created_on' => now()
+                        //         ];
+                        //     }
+
+                        //     $emergencyContacts = DB::table('tblemergency_contacts')->insert($dataSet);
+
+                        // // END: Emergency Contact Insert Query
+                    }
+                    
+
+                // END: Singles Encounter Update Query
+                
+                // if ($contactInfo->count() > 0 & $emergencyContacts === true) {
+                //     return response()->json([
+                //         'status' => 200,
+                //         'message' => 'Adding participant was successful!'
+                //     ]);
+                // } else {
+                //     return response()->json([
+                //         'status' => 500,
+                //         'message' => 'A problem was encountered while adding participant records. Please acontact system administrators.'
+                //     ]);
+                // }
+            }
+        } catch (\Throwable $th) {
+            // return response()->json([
+            //     'status' => 500,
+            //     'message' => 'Server Error: Please contact system adminstrator.'
+            // ]);
+            throw $th;
+        }
     }
 
     /**
