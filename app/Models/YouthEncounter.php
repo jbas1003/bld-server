8ed7fc42ba0df4ba1e104b8eec5faf9bcc7b6102
Jon <joncaesar@gmail.com>
2023-06-25 15:38:58 +0800

    Setup YE database and migration
    Setup YE database and migration

--- app/Models/YouthEncounter.php
+++ app/Models/YouthEncounter.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class YouthEncounter extends Model
 {
     use HasFactory;
+
+    protected $table = 'tblyouth_encounter';
+    protected $primaryKey = 'yeId';
+    protected $fillable = [
+        'member_id',
+        'room',
+        'tribe',
+        'nation',
+        'event_id',
+        'status',
+        'created_by',
+        'created_on'
+    ];
+
+    public $timestamps = false;
 }
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\OccupationController;
 use App\Http\Controllers\MemberStatusController;
 use App\Http\Controllers\ContactNumbersController;
 use App\Http\Controllers\MemberAccountsController;
+use App\Http\Controllers\YouthEncounterController;
 use App\Http\Controllers\SinglesEncounterController;
 
 /*
@@ -123,3 +124,9 @@ use App\Http\Controllers\SinglesEncounterController;
     Route::put('update_SE', [SinglesEncounterController::class, 'update']);
 
 // END: Singles Encounter Routes
+
+// START: Youth Encounter Routes
+
+    Route::post('show_YE', [YouthEncounterController::class, 'showYE']);
+
+// END: Youth Encounter Routes
