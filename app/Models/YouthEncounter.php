95ad8a06fa1aac8d9d6b1a491339e1554a140168
Jon <joncaesar@gmail.com>
2023-06-25 15:16:15 +0800

    Create YouthEncounter MRC
    Create YouthEncounter MRC (Model, Migration, and Controller)

---
+++ app/Models/YouthEncounter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YouthEncounter extends Model
+{
+    use HasFactory;
+}
